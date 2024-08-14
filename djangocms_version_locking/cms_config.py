6d9d4eb9792deee361ac627fce3832bab154c654
傅睿曼 <yxzfreeman@gmail.com>
2024-08-14 17:08:26 +0800

    remove Version.admin.locked monkeypatch.

--- djangocms_version_locking/cms_config.py
+++ djangocms_version_locking/cms_config.py
@@ -21,12 +21,6 @@ def add_alias_version_lock(obj, field):
 
 
 class VersionLockingCMSExtension(CMSAppExtension):
-    def __init__(self):
-        # The monkey patch is here to be sure that at module load time the Version class
-        # is registered and can be overriden without requiring a strict load order
-        # in the INSTALLED_APPS setting in a projects settings.py. This is why this patch
-        # Isn't loaded from: VersionLockingConfig.ready
-        from .monkeypatch import admin as monkeypatch_admin  # noqa: F401
 
     def configure_app(self, cms_config):
         pass
