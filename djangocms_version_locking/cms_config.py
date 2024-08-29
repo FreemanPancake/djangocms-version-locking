152e980759ad602392515e8068569cc90c74eaf3
傅睿曼 <yxzfreeman@gmail.com>
2024-08-29 10:29:36 +0800

    make state indicator readonly, and add more function to alias action list

--- djangocms_version_locking/cms_config.py
+++ djangocms_version_locking/cms_config.py
@@ -1,5 +1,5 @@
-from django.template.loader import render_to_string
 from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 
 from cms.app_base import CMSAppConfig, CMSAppExtension
 
@@ -12,7 +12,7 @@ def add_alias_version_lock(obj, field):
     version = obj.versions.all()[0]
     lock_icon = ""
     if version.state == DRAFT and version_is_locked(version):
-        lock_icon = render_to_string("djangocms_version_locking/admin/locked_mixin_icon.html")
+        lock_icon = mark_safe('<span class="cms-icon cms-icon-lock"></span>')
     return format_html(
         "{is_locked}{field_value}",
         is_locked=lock_icon,
@@ -21,6 +21,13 @@ def add_alias_version_lock(obj, field):
 
 
 class VersionLockingCMSExtension(CMSAppExtension):
+    def __init__(self):
+        # The monkey patch is here to be sure that at module load time the Version class
+        # is registered and can be overriden without requiring a strict load order
+        # in the INSTALLED_APPS setting in a projects settings.py. This is why this patch
+        # Isn't loaded from: VersionLockingConfig.ready
+        from .monkeypatch import admin as monkeypatched_version_admin  # noqa: F401
+        from .monkeypatch.djangocms_alias import admin as monkeypatched_alias_admin  # noqa: F401
 
     def configure_app(self, cms_config):
         pass
---
+++ djangocms_version_locking/monkeypatch/admin.py
@@ -0,0 +1,37 @@
+import json
+
+from django.template.loader import render_to_string
+from django.utils.translation import gettext_lazy as _
+
+from djangocms_versioning.admin import StateIndicatorMixin
+from djangocms_versioning.constants import INDICATOR_DESCRIPTIONS
+from djangocms_versioning.indicators import content_indicator, content_indicator_menu
+from djangocms_versioning.helpers import get_latest_admin_viewable_content
+
+def _get_indicator_column(func):
+    '''
+    Change the State Indicator to readonly, publish process will be take over by djangocms-moderation.
+    '''
+    def inner(self, request):
+        def indicator(obj):
+            if self._extra_grouping_fields is not None:  # Grouper Model
+                content_obj = get_latest_admin_viewable_content(obj, include_unpublished_archived=True, **{
+                    field: getattr(self, field) for field in self._extra_grouping_fields
+                })
+            else:  # Content Model
+                content_obj = obj
+            status = content_indicator(content_obj)
+            return render_to_string(
+                "admin/djangocms_versioning/indicator.html",
+                {
+                    "state": status or "empty",
+                    "description": INDICATOR_DESCRIPTIONS.get(status, _("Empty")),
+                    "menu_template": "admin/cms/page/tree/indicator_menu.html",
+                }
+            )
+        indicator.short_description = self.indicator_column_label
+        return indicator
+    return inner
+
+
+StateIndicatorMixin.get_indicator_column = _get_indicator_column(StateIndicatorMixin.get_indicator_column)
