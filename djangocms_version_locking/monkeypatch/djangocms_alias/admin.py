acd9871ba0d49dff1de25a44fde26c38e7f29692
傅睿曼 <yxzfreeman@gmail.com>
2024-10-25 14:01:26 +0800

    fix Alias language filer selector UI issue.

--- djangocms_version_locking/monkeypatch/djangocms_alias/admin.py
+++ djangocms_version_locking/monkeypatch/djangocms_alias/admin.py
@@ -14,6 +14,9 @@ from djangocms_versioning.helpers import (
 
 
 class AliasAdmin(OriginalAliasAdmin):
+
+    change_list_template = "monkeypatch/cms/admin/cms/grouper/change_list.html"
+
     def get_actions_list(self) -> list:
         """Add alias manage version link"""
         original_list = super().get_actions_list()
