92756574e33098a2b46876c10aff20f256319a03
Josh Yu <joshyu@foxmail.com>
2024-11-08 09:17:56 +0800

    chore: comment models

--- djangocms_version_locking/models.py
+++ djangocms_version_locking/models.py
@@ -1,12 +1,12 @@
-from django.conf import settings
-from django.db import models
-from django.utils.translation import gettext_lazy as _
-
-from djangocms_versioning.models import Version
-
-
 # NOTE: 'version lock' has been migrated to djangocms_versioning.Version table 'locked_by' field.
 # so this table is not needed anymore after data migration.
+
+# from django.conf import settings
+# from django.db import models
+# from django.utils.translation import gettext_lazy as _
+
+# from djangocms_versioning.models import Version
+
 
 # class VersionLock(models.Model):
 #     created = models.DateTimeField(auto_now_add=True)
