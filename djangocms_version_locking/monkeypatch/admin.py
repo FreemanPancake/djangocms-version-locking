25dddceebb9907a8b7f8e55c0c0dd82c52870eaa
FreemanPancake <yxzfreeman@gmail.com>
2024-11-19 16:05:51 +0800

    fix: remove duplicated media js from monkeypatch.

--- djangocms_version_locking/monkeypatch/admin.py
+++ djangocms_version_locking/monkeypatch/admin.py
@@ -73,7 +73,7 @@ ChangeListActionsMixin.Media.css["all"] += (
     static_with_version("cms/css/cms.pagetree.css"),
     "djangocms_version_locking/css/actions.css",
 )
-ChangeListActionsMixin.Media.js += ("admin/js/jquery.init.js", "djangocms_version_locking/js/actions.js",)
+ChangeListActionsMixin.Media.js += ("djangocms_version_locking/js/actions.js",)
 
 ExtendedVersionAdminMixin._get_manage_versions_link = _get_manage_versions_link
 ExtendedVersionAdminMixin.get_actions_list = _get_actions_list(ExtendedVersionAdminMixin.get_actions_list)
